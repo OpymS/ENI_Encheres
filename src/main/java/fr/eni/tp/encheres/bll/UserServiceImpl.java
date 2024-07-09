e9dd6215e7aeab2d3330527ab0b731620e6f9a40
AmelieAMAILLAND <167514749+AmelieAMAILLAND@users.noreply.github.com>
2024-07-09 16:43:32 +0200

    fix list

--- src/main/java/fr/eni/tp/encheres/bll/UserServiceImpl.java
+++ src/main/java/fr/eni/tp/encheres/bll/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package fr.eni.tp.encheres.bll;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.security.crypto.password.PasswordEncoder;
